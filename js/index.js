3601717db9dd6e07edea8c3d67891307c9935200
ecomeel <ecomeel@gmail.com>
2023-06-20 13:38:52 +0300

    red color error fixed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const VIEW_BTN_CLASSNAME = 'movie__check-view';
 const VIEWED_MOVIE_CLASSNAME = 'movie_viewed';
 const DELETE_BTN_CLASSNAME = 'movie__delete';
 const VIOLET_BACKGROUND_COLOR_CLASSNAME = 'violet-back'
-const EMPTY_INPUT_ERROR_CLASSNAME = 'red-color';
+const EMPTY_INPUT_ERROR_CLASSNAME = 'red-border';
 const STORAGE_MOVIES_LABEL = 'movies';
 
 const newMovieInputNode = document.getElementById('addMovieInput');
